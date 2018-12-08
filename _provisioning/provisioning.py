761ccc90d84885ae452e08fb77ce262dcc011e08
rkgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-12-08 13:18:45 +0900

    Add install pip3 and django   - append provision command to Vagrantfile

--- _provisioning/provisioning.py
+++ _provisioning/provisioning.py
@@ -4,11 +4,12 @@ import subprocess
 
 
 def _preprocess():
-    subprocess.Popen(['pwd'])
 
+    # install pip3
     subprocess.run(['apt-get', 'update'])
     subprocess.run(['apt-get', 'install', 'python3-pip', '-y'])
 
+    # install django
     subprocess.run(['pip3', 'install', 'django'])
 
 
